1d278e93a96d44a8f81bf4f5bb1e6f57384092a4
Spencer Wood <spencer.wood@me.com>
2020-03-01 20:44:06 -0600

    added requires comments to the index.js, created the DB and tables needed for assignment

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
+//Variables for the project
 const inquire = require('inquirer');
 const consoleTable = require('consle.table');
 const figlet = require('figlet');
 const mysql = require('mysql');
+
+//mysql connection
+//commands to connect to mysql and run functions
+
+
+
+//inquirer Prompts about what user wants to do
+
+
+
+//=====================================
+//===========Objectives================
+//add departments
+//add roles
+//add employees
+//update employee roles
+
+//BONUS
+//update employee mnagers
+//view employees by manager
+//delete departs, roles, and employees
+//view total utilized budget of a department -- ie the combined salaries
+
+
+
