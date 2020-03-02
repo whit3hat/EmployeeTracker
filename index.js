6ce3b710172571b15630253f3e93e0a505ca5497
Spencer Wood <spencer.wood@me.com>
2020-03-01 20:47:16 -0600

    created connection to db and added info for connection

--- index.js
+++ index.js
@@ -5,8 +5,27 @@ const figlet = require('figlet');
 const mysql = require('mysql');
 
 //mysql connection
-//commands to connect to mysql and run functions
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "Madylin3!",
+    database: "companyinfo_db"
+  });
 
+
+//connect to the mysql server and sql DB
+connection.connect(function(err) {
+    if (err) throw err;
+    //run the start function after the connection is made to prompt the user
+    start();
+});
 
 
 //inquirer Prompts about what user wants to do
